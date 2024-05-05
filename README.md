b0daeae8d0f57b241097ff8b29001e077386ac94
Pierre Rossel <>
2024-05-05 23:36:06 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# SuetoneDevice
+
+Code de l'objet Suetone, pour DFRobot Firebeetle 2 ESP32-S3.
+
+Un projet 2024 de [Basile Brun](https://github.com/chap0ng/2023-head-md-future-of-drawing/tree/main/expo) / HEAD-Genève / master Media Design
+
+## Installation
+
+* Arduino IDE
+* Board esp32
+* Librairie  [DFRobot_AXP313A](https://github.com/cdjq/DFRobot_AXP313A)
+
+Plus d'infos sur https://wiki.dfrobot.com/SKU_DFR0975_FireBeetle_2_Board_ESP32_S3_Advanced_Tutorial#target_9
+
+## Configuration
+
+Tools > Board manager: DFRobot Firebeetle 2 ESP32-S3
+
+Tools > Port: your port
+
+Pour avoir les messages sur le port série:
+
+Tools > USB CDC on boot: Enabled
+
+Tools > Flash size: 16 MB
--- Suetone.ino
+++ Suetone.ino
@@ -1,28 +1,10 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
+    Suetone code for Firebeetle 2 ESP32-S3
+    
+    BLE Inspired from https://wiki.dfrobot.com/SKU_DFR0975_FireBeetle_2_Board_ESP32_S3_Advanced_Tutorial#target_2
 
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
+    Pierre Rossel   2024-05-05   Initial version
 */
-
-/* This example domenstrates the Bluetooth data transparent transmission function. Burn the code, open serial monitor, turn on the BLE debugger on the phone, then,
- * 1. you can see the data sent by ESP32-S3--see APP usage image
- * 2. send data to ESP32-S3 by the input box of BLE debugger--see APP usage image
- * This example originates from BLE_uart sample
- */
 
 #include <BLE2902.h>
 #include <BLEDevice.h>
